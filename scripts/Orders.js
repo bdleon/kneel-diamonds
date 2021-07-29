979cd32fbecff832c270f4fbc24789bd924729d8
Brandon De Leon <brandondeleon.615@gmail.com>
2021-07-29 13:04:32 -0500

    added function to add the total order in order.js

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -1,9 +1,39 @@
 
-import { getCustomOrders } from "./database.js"
+import { getCustomOrders, getMetals, getSizes, getStyles } from "./database.js"
+
+const metals = getMetals()
+const sizes = getSizes()
+const styles = getStyles()
+
+
 
 const buildOrderListItem = (order) => {
+
+    const foundMetal = metals.find(
+        (metal) => {
+            return metal.id === order.metalId
+        }
+    )
+    const foundSize = sizes.find(
+        (size) => {
+            return size.id === order.sizeId
+        }
+    )
+    const foundStyle = styles.find(
+        (style) => {
+            return style.id === order.styleId
+        }
+    )
+    const totalCost = foundMetal.price + foundStyle.price + foundSize.price
+    
+    const costString = totalCost.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD"
+    })
+    
+
     return `<li>
-        Order #${order.id} was placed on ${order.timestamp}
+        Order #${order.id} cost ${costString} was placed on ${order.timestamp}
     </li>`
 }
 
